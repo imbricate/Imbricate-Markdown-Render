2a8709b3a4228819e621e04feb94063378bbcff0
wmxpy <wm@mengw.io>
2024-05-07 01:30:37 -0500

    feat: Add e2e test for rendering encoded HTML

---
+++ test/e2e/encoded-html.test.ts
@@ -0,0 +1,24 @@
+/**
+ * @author WMXPY
+ * @namespace MarkdownRender
+ * @description Encoded HTML
+ * @override E2E Test
+ */
+
+import { renderMarkdownToHtml } from "../../src/markdown-to-html";
+
+describe("Given (Encoded HTML) Cases", (): void => {
+
+    test("should be able to highlight unknown language", async (): Promise<void> => {
+
+        const source: string = [
+            "```html",
+            "<div>test</div>",
+            "```",
+        ].join("\n");
+
+        const parsed: string = await renderMarkdownToHtml(source);
+
+        expect(parsed).toContain("<pre><code>");
+    });
+});
